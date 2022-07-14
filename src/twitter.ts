299280d5d92ca786fbfd802f5d2ab1f390b1bdc3
dzgoldman <dzgoldman@wesleyan.edu>
2022-07-14 14:17:13 -0400

    allow devnet or testnet

--- src/twitter.ts
+++ src/twitter.ts
@@ -140,7 +140,7 @@ const extractAddress = (str: string): string=> {
 }
 
 const isFaucetRequest = (tweetText: string): boolean=>{
-    return tweetText.toLowerCase().includes("nitro devnet")
+    return tweetText.toLowerCase().includes("nitro devnet") || tweetText.toLowerCase().includes("nitro testnet")
 }
 
 
