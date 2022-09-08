22ddd5fb94ff00034ad2fd4219dc57a46824fd51
Jason_Wanxt <wanxutao123@gmail.com>
2022-09-08 14:17:56 +0800

    change error handle

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,13 @@ const main = async () => {
     switch (argv.action) {
         case "start":
             if(!argv.chainid) {
-                console.error("Error: arg chainid needed");
-                exit(1);
+                throw new Error("Error: arg chainid needed");
             }
             return startFaucetProcess();
-            // "start": "tsc && node dist/index.js",
-            // "get_token": "tsc && node dist/scripts/get_token.js",
+
         case "get_token":
-            // `${env.consumerKey}:${env.consumerSecret}`
-            if(!env.consumerKey || !env.consumerSecret) {
-                if(!env.consumerKey) console.error("Error: env CONSUMER_KEY needed");
-                if(!env.consumerSecret) console.error("Error: env CONSUMER_SECRET needed");
-            }
+            if(!env.consumerKey) throw new Error("Error: env CONSUMER_KEY needed");
+            if(!env.consumerSecret) throw new Error("Error: env CONSUMER_SECRET needed");
             return getToken();
 
         default:
